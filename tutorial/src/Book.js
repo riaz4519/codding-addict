763a15df93f2e8f3450b749f8908acdc6b0708da
riaz4519 <riaz.i3216@gmail.com>
2023-08-17 21:21:48 +0600

    after 34 of codding addict

---
+++ tutorial/src/Book.js
@@ -0,0 +1,12 @@
+const Book = ({ img, title, author }) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+
+      <h4> {author} </h4>
+    </article>
+  )
+}
+
+export default Book
--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,21 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Curious Minds',
-    img: './images/book_1.jpg',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-    id: 2,
-  },
-]
+import { books } from './books'
+import Book from './Book'
 
 const BookList = () => {
   return (
@@ -24,16 +11,6 @@ const BookList = () => {
         return <Book {...book} key={book.id} />
       })}
     </section>
-  )
-}
-
-const Book = ({ img, title, author }) => {
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4> {author} </h4>
-    </article>
   )
 }
 
