913279b2a0ba6b14f594e62dcb10cc8b680d39c9
riaz4519 <riaz.i3216@gmail.com>
2023-09-21 12:06:39 +0600

    74-85

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,25 @@
-const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
-};
+import { useEffect, useState } from 'react'
 
-export default CleanupFunction;
+const CleanupFunction = () => {
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle Component
+      </button>
+
+      {toggle && <RandomComponent />}
+    </div>
+  )
+}
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log('logged from random component')
+  })
+
+  return <h1>Hello There</h1>
+}
+
+export default CleanupFunction
